bba3a6452762ebf2b268d5a28b26e91636a08618
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-08 22:36:05 +0300

    123

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = fs.readFileSync(PATH_DB, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    return [];
+  }
+};
+readContacts();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  try {
+    const data = JSON.stringify(contacts, null, 2);
+    fs.writeFileSync(PATH_DB, data, 'utf-8');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+  }
+};
